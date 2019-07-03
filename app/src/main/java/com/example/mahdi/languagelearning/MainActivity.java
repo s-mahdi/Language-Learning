b9da1313320f643cf876ebab015561853915d08d
Mahdi Hoseini <34459301+smmh1376@users.noreply.github.com>
2019-07-03 12:56:35 +0430

    1.MainActivity Recycler added. 2.Local DB added. i had used Room Library for it
    Signed-off-by: Mahdi Hoseini <34459301+smmh1376@users.noreply.github.com>

--- app/src/main/java/com/example/mahdi/languagelearning/MainActivity.java
+++ app/src/main/java/com/example/mahdi/languagelearning/MainActivity.java
@@ -2,17 +2,93 @@ package com.example.mahdi.languagelearning;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProviders;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.mahdi.languagelearning.data.Tables.Word;
+import com.example.mahdi.languagelearning.data.WordViewModel;
+import com.example.mahdi.languagelearning.recyclerView.RecyclerViewClickListener;
+import com.example.mahdi.languagelearning.recyclerView.WordListAdapter;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import java.util.List;
+import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static final int NEW_WORD_ACTIVITY_REQUEST_CODE = 1;
+
+    private WordViewModel mWordViewModel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Intent i = new Intent(this, IntroActivity.class);
-        startActivity(i);
+        Objects.requireNonNull(getSupportActionBar()).hide();
+
+        /*Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);*/
+
+        mWordViewModel = ViewModelProviders.of(this).get(WordViewModel.class);
+
+        RecyclerView recyclerView = findViewById(R.id.mainActivityRecyclerView);
+
+        RecyclerViewClickListener listener = new RecyclerViewClickListener() {
+            @Override
+            public void onClick(View view, int position) {
+                // TODO: 03/07/2019 connect to training class
+                Toast.makeText(MainActivity.this, "clicked", Toast.LENGTH_LONG).show();
+                Word clickedWord = Objects.requireNonNull(mWordViewModel.getAllWords().getValue()).get(position);
+                mWordViewModel.setPassed(clickedWord);
+            }
+        };
+
+        final WordListAdapter adapter = new WordListAdapter(this, listener);
+        recyclerView.setAdapter(adapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+
+        mWordViewModel.getAllWords().observe(this, new Observer<List<Word>>() {
+            @Override
+            public void onChanged(@Nullable final List<Word> words) {
+                // Update the cached copy of the words in the adapter.
+                adapter.setWords(words);
+            }
+        });
+
+
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, NewWordActivity.class);
+                startActivityForResult(intent, NEW_WORD_ACTIVITY_REQUEST_CODE);
+            }
+        });
+
+
+    }
+
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == NEW_WORD_ACTIVITY_REQUEST_CODE && resultCode == RESULT_OK) {
+            Word word = new Word(data.getStringExtra(NewWordActivity.EXTRA_REPLY),
+                    data.getBooleanExtra(NewWordActivity.EXTRA_REPLY_PASSED, false));
+            mWordViewModel.insert(word);
+        } else {
+            Toast.makeText(
+                    getApplicationContext(),
+                    R.string.empty_not_saved,
+                    Toast.LENGTH_LONG).show();
+        }
     }
 }
